e3c37c0b428673dae910144aef93812acea58805
Bhanu <bhanublez@gmail.com>
2022-10-01 00:35:03 +0530

    Second commit

---
+++ Day10/Scan_int_double_String.java
@@ -0,0 +1,22 @@
+//Scanning and printing double,int and String
+/*
+If you use the nextLine() method immediately following the nextInt() method,
+ recall that nextInt() reads integer tokens; because of this, the last newline 
+character for that line of integer input is still queued in the input buffer and 
+the next nextLine() will be reading the remainder of the integer line (which is empty).
+*/
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d=scan.nextDouble();
+        String s=scan.nextLine();
+        s= scan.nextLine();    
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
---
+++ Day10/Scanner_hasNext.java
@@ -0,0 +1,17 @@
+//Use of Scannner.hasNext();
+import java.io.*;
+import java.util.*;
+
+public class EndOfFile {
+
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        for(int i=1;sc.hasNext();i++) {
+            String s=sc.nextLine();
+            System.out.println(i + " " + s);
+           
+        }
+    }
+}
